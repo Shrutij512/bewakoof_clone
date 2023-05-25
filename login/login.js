2e2e6db3b29b07ac4fd2b2209254d14f6c69ff59
venkata raghavendra abhishek <ragavaabhishek55@gmail.com>
2023-05-25 20:05:31 +0530

    static part completed

---
+++ login/login.js
@@ -0,0 +1,69 @@
+
+let loadVal = JSON.parse(localStorage.getItem("userdetails"))
+let noarr = JSON.parse(localStorage.getItem("mobile_no")) || [];
+document.querySelector("#cont_btn").addEventListener("click", myfunction);
+function myfunction() {
+
+    let no = document.getElementById("input_for_no").value;
+
+    if (no == "") {
+        document.getElementById("mobile_no").style.border = "1px solid red"
+        document.getElementById("show_wrong_p").innerText = "Mobile Number is required"
+    }
+    else {
+
+        if (loadVal == null) {
+            let obj = {
+                number: no
+            }
+            noarr.push(obj)
+            localStorage.setItem("mobile_no", JSON.stringify(noarr))
+            window.location.href = "signup.html"
+        }
+
+        else {
+             checkno(loadVal, no)
+        }
+
+
+
+
+
+    }
+}
+function checkno( loadVal,no) {
+    loadVal.map(function (elem) {
+
+        if (no == elem.mobile) {
+           
+            let obj = {
+                number: no
+            }
+            noarr.push(obj)
+            localStorage.setItem("mobile_no", JSON.stringify(noarr))
+            window.location.href = "otp.html"
+           let x=  document.getElementById("show_wrong_p");
+           x.innerText = "Enter otp"
+        } else {
+           
+            let obj = {
+                number: no
+            }
+            noarr.push(obj)
+            localStorage.setItem("mobile_no", JSON.stringify(noarr))
+
+            window.location.href = "signup.html"
+            let x=  document.getElementById("show_wrong_p");
+           x.innerText = "New User Sign Up first"
+           
+
+        }
+    });
+
+}
+let Namearr = JSON.parse(localStorage.getItem("username")) ;
+ Namearr.map(function(ele){
+    document.getElementById("login_text").innerText=ele.name;
+ })
+
+
