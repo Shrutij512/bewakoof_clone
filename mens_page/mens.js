35571031080239cbe6356b14fa1aefac0721afec
Vishal Sharma <svsharma918277@gmail.com>
2023-05-25 23:35:06 +0530

    adding mens page

---
+++ mens_page/mens.js
@@ -0,0 +1,28 @@
+
+
+var products=[
+    {
+        image_url:"https://images.bewakoof.com/t640/men-s-purple-beast-within-2-0-oversized-t-shirt-581488-1677228989-1.jpg",
+        product_info:"Men's Purple Beast Within Graphic Printed Oversized T-shirt",
+        price:"1499",
+        discounted_price:"799"
+        members_price: price * (.001),
+
+    }, {
+        image_url:"https://images.bewakoof.com/t640/men-s-blue-the-ryuk-graphic-printed-oversized-t-shirt-592486-1684488819-1.jpg",
+        product_info:"Men's Blue The Ryuk Graphic Printed Oversized T-shirt",
+        price:"1299",
+        discounted_price:"599"
+        members_price: price*.001,
+    },  {
+        image_url:"https://images.bewakoof.com/t640/men-s-blue-the-ryuk-graphic-printed-oversized-t-shirt-592486-1684488819-1.jpg",
+        product_info:"Men's Blue The Ryuk Graphic Printed Oversized T-shirt",
+        price:"1299",
+        discounted_price:"599"
+        members_price: price*.001,
+    }
+
+
+]
+
+
